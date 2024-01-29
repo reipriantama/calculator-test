5db5b59138b3aeab679915f21aa88360ccdde48e
reipriantama <priantamareinato@gmail.com>
2024-01-29 15:23:00 +0700

    add

--- src/components/CalculatorScreen.jsx
+++ src/components/CalculatorScreen.jsx
@@ -6,7 +6,7 @@ const CalculatorScreen = ({ number }) => {
       <div className='p-2 flex justify-end h-20 border-2 shadow-lg border-indigo-400 grid content-center rounded'>
         <div className='text-2xl'>
           {' '}
-          {number}12 + 12 = 724 
+          {number} 123123123
         </div>
       </div>
     </div>
