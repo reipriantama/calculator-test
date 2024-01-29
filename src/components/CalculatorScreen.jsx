d711e1715b7c2c4dc97e637e41ebf83f23059230
reipriantama <priantamareinato@gmail.com>
2024-01-29 22:21:34 +0700

    result feature

--- src/components/CalculatorScreen.jsx
+++ src/components/CalculatorScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CalculatorScreen = ({ number }) => {
+const CalculatorScreen = ({ number, result }) => {
   return (
     <div className="grid content-center justify-end h-20 p-2 mt-5 border-2 border-indigo-400 rounded shadow-lg">
       <div className="text-2xl"> {number} </div>
