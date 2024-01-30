65a12c266cfd2d4eb64d167fbc741a038683f7e7
reipriantama <priantamareinato@gmail.com>
2024-01-30 13:14:31 +0700

    feature equal

--- src/components/CalculatorScreen.jsx
+++ src/components/CalculatorScreen.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const CalculatorScreen = ({ number, result }) => {
+const CalculatorScreen = ({ number }) => {
   return (
-    <div className="grid content-center justify-end h-20 p-2 mt-5 border-2 border-indigo-400 rounded shadow-lg">
-      <div className="text-2xl"> {number} </div>
+    <div className='grid content-center justify-end h-20 p-2 mt-5 mb-5 text-white border-2 border-indigo-100 rounded shadow-lg'>
+      <div className='text-4xl'> {number} </div>
     </div>
   );
 };
